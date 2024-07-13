aa5118a168794f71e5974c899e199dc2ce404877
Jens Brandenburg <jens@vragments.com>
2024-07-13 18:06:55 +0200

    renamed manifest, disabled serviceworker

--- src/index.js
+++ src/index.js
@@ -6,3 +6,14 @@ const root = createRoot(container);
 root.render(<App />);
 
 document.__isRootWindow = true;
+
+// if ("serviceWorker" in navigator) {
+//     navigator.serviceWorker
+//         .register(new URL("./service-worker.js", import.meta.url), { type: "module", scope: "." })
+//         .then((serviceWorker) => {
+//             console.log("Service Worker registered: ", serviceWorker);
+//         })
+//         .catch((error) => {
+//             console.error("Error registering the Service Worker: ", error);
+//         });
+// }
--- src/views/App.tsx
+++ src/views/App.tsx
@@ -7,10 +7,10 @@ import WhichPlates from "../components/WhichPlates";
 const App = () => {
     const targetWeightState = useState<number | null>(null);
 
-    /* First run: */
-    useEffect(() => {
-        navigator.serviceWorker.register(new URL("../service-worker.js", import.meta.url), { type: "module", scope: "." });
-    }, []);
+    // /* First run: */
+    // useEffect(() => {
+    //     navigator.serviceWorker.register(new URL("../service-worker.js", import.meta.url), { type: "module", scope: "." });
+    // }, []);
 
     return (
         <div className="mx-2 my-2 w-fit sm:mx-auto sm:my-6 sm:w-3/4 lg:w-3/5">
